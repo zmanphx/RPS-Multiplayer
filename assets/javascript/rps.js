869cc4978a1c4d433eb1a499b355bb52db1e1a91
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-03-29 20:04:24 -0700

    commit

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -14,7 +14,7 @@ var database = firebase.database();
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
 var whichConnect = 0;
-var isClicked= false;// insures button value only records once. 
+var isClicked = false; // insures button value only records once.
 //When player connects
 connectedRef.on("value", function(snap) {
   if (snap.val()) {
@@ -33,6 +33,8 @@ connectionsRef.on("value", function(snap) {
 
   if (snap.numChildren() == 1) {
     whichConnect = 1;
+    database.ref("Players/player2").update({ message1: "" });
+    database.ref("Players/player1").update({ message1: "" });
     database.ref("Players/player1").update({ Name: "" });
     database.ref("Players/player2").update({ Name: "" });
     database.ref("Players/player2").update({ Win: "0" });
@@ -80,6 +82,8 @@ $("#submitName").on("click", function() {
       $("#P1Name").text(pName);
       playerNum = 1;
       $("#idHolder1").text("You are player 1!");
+      database.ref("Players/player2").update({ message1: "" });
+    database.ref("Players/player1").update({ message1: "" });
       database.ref("Players/player2").update({ Name: "" });
       database.ref("Players/player2").update({ Win: "0" });
       database.ref("Players/player1").update({ Win: "0" });
@@ -105,62 +109,68 @@ database.ref("Players").on("value", function(snapshot) {
   $("#P2score").text(response.player2.Win);
   $("#P1Name").text(response.player1.Name);
   $("#P2Name").text(response.player2.Name);
+  $("#P1WL").text(response.player1.message1);
+  $("#P2WL").text(response.player2.message1);
 });
 
 // player chooses one of the p,r,s
 
 $("#paperImg").on("click", function() {
   event.preventDefault();
-  if( isClicked){ return;}
+  if (isClicked) {
+    return;
+  }
   isClicked = true;
+
   if (playerNum == 1 || playerNum == 2) {
     if (playerNum == 1) {
       database.ref("Players/player1").update({ ClickVal: "p" });
+      database.ref("Players/player1").update({ message1: "player 1 clicked" });
     } else {
       database.ref("Players/player2").update({ ClickVal: "p" });
+      database.ref("Players/player2").update({ message1: "player 2 clicked" });
     }
   }
-
 });
 
 $("#scissorImg").on("click", function() {
   event.preventDefault();
-  if (isClicked){return;}
- isClicked = true;
- if (playerNum == 1 || playerNum == 2) {
+  if (isClicked) {
+    return;
+  }
+  isClicked = true;
+
+  if (playerNum == 1 || playerNum == 2) {
     if (playerNum == 1) {
       database.ref("Players/player1").update({ ClickVal: "s" });
+      database.ref("Players/player1").update({ message1: "player 1 clicked" });
     } else {
       database.ref("Players/player2").update({ ClickVal: "s" });
+      database.ref("Players/player2").update({ message1: "player 2 clicked" });
     }
   }
- 
-
 });
 
 $("#rockImg").on("click", function() {
   event.preventDefault();
-  if (isClicked){ return;}
-    isClicked = true;
- if (playerNum == 1 || playerNum == 2) {
+  if (isClicked) {
+    return;
+  }
+  isClicked = true;
+
+  if (playerNum == 1 || playerNum == 2) {
     if (playerNum == 1) {
       database.ref("Players/player1").update({ ClickVal: "r" });
+      database.ref("Players/player1").update({ message1: "player 1 clicked" });
     } else {
       database.ref("Players/player2").update({ ClickVal: "r" });
+      database.ref("Players/player2").update({ message1: "player 2 clicked" });
     }
   }
- 
-
 });
 
-
-$("#submitChat").on("click", function(){
-   event.preventDefault();
-  
-
-
-
-
+$("#submitChat").on("click", function() {
+  event.preventDefault();
 });
 
 database.ref("Players").on("value", function(snapshot) {
@@ -170,86 +180,97 @@ database.ref("Players").on("value", function(snapshot) {
 
   var p1Pick = response.player1.ClickVal;
   var p2Pick = response.player2.ClickVal;
-  
-
 
   if (prsArr.indexOf(p1Pick) > -1 && prsArr.indexOf(p2Pick) > -1) {
     $("#idHolder1").text("Player1 :" + p1Pick + " Player2 " + p2Pick);
-  
+
     // now do the logic after both players have picked.
     //*********************************************************** */
-    database.ref("Players/player1").update({ClickVal:""});
-    database.ref("Players/player2").update({ClickVal:""});
+    database.ref("Players/player1").update({ ClickVal: "" });
+    database.ref("Players/player2").update({ ClickVal: "" });
 
+    $("#P1choise").text("");
+    $("#P2choise").text("");
 
+    if (p1Pick === p2Pick) {
+      var dispText =
+        response.player1.Name +
+        " chose " +
+        rpsTranslate(p1Pick) +
+        ", " +
+        response.player2.Name +
+        " chose " +
+        rpsTranslate(p2Pick) +
+        "  Tie!!!";
 
-      if (p1Pick === p2Pick) {
-        
-     
-         var dispText=  response.player1.Name +" chose "+ rpsTranslate(p1Pick) + ", " + response.player2.Name +" chose " +
-         rpsTranslate(p2Pick) + "  Tie!!!";
-         
-         $("#idHolder1").text(dispText);
-        
-         var ties= parseInt( response.rps_session.game_ties);
-            ties++;
-         database.ref("rps_session").update({game_ties: ties});
+      $("#idHolder1").text(dispText);
 
+      var ties = parseInt(response.rps_session.game_ties);
+      ties++;
+      database.ref("rps_session").update({ game_ties: ties });
+    } else if (
+      (p1Pick === "r" && p2Pick === "p") ||
+      (p1Pick === "s" && p2Pick === "r") ||
+      (p1Pick === "p" && p2Pick === "s")
+    ) {
+      //Player2 wins
+      var p2win = parseInt(response.player2.Win);
+      p2win++;
+      database.ref("Players/player2").update({ Win: p2win });
+      var dispText =
+        response.player1.Name +
+        " chose " +
+        rpsTranslate(p1Pick) +
+        ", " +
+        response.player2.Name +
+        " chose " +
+        rpsTranslate(p2Pick) +
+        ": " +
+        response.player2.Name +
+        " Wins!!";
+      $("#idHolder1").text(dispText);
+    } else {
+      //Play1 wins
 
-      } else if (
-        (p1Pick === "r" && p2Pick === "p") ||
-        (p1Pick === "s" && p2Pick === "r") ||
-        (p1Pick === "p" && p2Pick === "s")
-      ) { //Player2 wins
-           var p2win =  parseInt( response.player2.Win);
-           p2win++;
-           database.ref("Players/player2").update({Win:p2win});
-           var dispText=  response.player1.Name +" chose "+ rpsTranslate(p1Pick) + ", " + response.player2.Name +" chose " +
-           rpsTranslate(p2Pick) + ": " + response.player2.Name + " Wins!!";
-           $("#idHolder1").text(dispText);
+      var p1win = parseInt(response.player1.Win);
+      p1win++;
+      database.ref("Players/player1").update({ Win: p1win });
+      var dispText =
+        response.player1.Name +
+        " chose " +
+        rpsTranslate(p1Pick) +
+        ", " +
+        response.player2.Name +
+        " chose " +
+        rpsTranslate(p2Pick) +
+        ": " +
+        response.player1.Name +
+        " Wins!!";
+      $("#idHolder1").text(dispText);
+    }
 
-       
-      } else {  //Play1 wins
-                 
-        var p1win =  parseInt( response.player1.Win);
-        p1win++;
-        database.ref("Players/player1").update({Win:p1win});
-        var dispText=  response.player1.Name +" chose "+ rpsTranslate(p1Pick) + ", " + response.player2.Name +" chose " +
-        rpsTranslate(p2Pick) + ": " + response.player1.Name + " Wins!!";
-        $("#idHolder1").text(dispText);
-
-
-        
-      }
-    
-    
- 
- 
+    database.ref("Players/player2").update({ message1: "Play Again" });
+    database.ref("Players/player1").update({ message1: "Play Again" });
     isClicked = false;
- 
- 
- 
- 
-
-
-
-  //******************************************************************** */  
+    
+    setTimeout(function(){  
+      $("#idHolder1").text("Choose Image for Paper Scissors Rock");
+  
+  
+  }, 7000);
+    //******************************************************************** */
   }
 });
 
-
-
-
-
-function rpsTranslate(pick){
-
-  if (pick=="p") { return "paper";}
-    else
-    if (pick=="s") { return "scissors";}
-    else
-    if (pick =="r"){return "rock";}
+function rpsTranslate(pick) {
+  if (pick == "p") {
+    return "paper";
+  } else if (pick == "s") {
+    return "scissors";
+  } else if (pick == "r") {
+    return "rock";
+  }
 }
-
 
 function rpsFunction(p1, p2) {
   var rpsVal = p1 + p2;
